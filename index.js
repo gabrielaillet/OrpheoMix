3721c133a53a557fc13ad0338754eb4369d224c6
Aillet Gabriel <aillet.gabriel05@gmail.com>
2025-01-16 09:42:32 +0100

    artist page

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ const server = http.createServer((req, res) => {
   if (req.url === "/" || req.url === "/index.html") {
     // Serve the index page
     serveFile(res, "index.html", "text/html");
-
-
   } else if (req.url === "/css/index.css") {
     // Serve the CSS for the index page
     serveFile(res, "/css/index.css", "text/css");
@@ -44,33 +42,48 @@ const server = http.createServer((req, res) => {
   } else if (req.url === "/css/bootstrap.min.css") {
     // Serve the CSS for the next page
     serveFile(res, "/css/bootstrap.min.css", "text/css");
-  } 
-  
-  
-  
-  else if (req.url === "/firstpage.html") {
+  } else if (req.url === "/firstpage.html") {
     // Serve the first page
     serveFile(res, "/html/firstPage.html", "text/html");
-
   } else if (req.url === "/nextpage.html") {
     // Serve the next page
     serveFile(res, "/html/nextPage.html", "text/html");
-  }
-  else if (req.url === "/js/buttonGroup.js") {
+  } else if (req.url === "/js/buttonGroup.js") {
     // Serve the button group JavaScript file
     serveFile(res, "js/buttonGroup.js", "application/javascript");
-  }   else if (req.url === "/js/bootstrap.bundle.min.js") {
+  } else if (req.url === "/js/bootstrap.bundle.min.js") {
     // Serve the button group JavaScript file
     serveFile(res, "/js/bootstrap.bundle.min.js", "application/javascript");
-  } 
+  } else if (req.url.startsWith("/assets/")) {
+    // Serve assets (images, etc.)
+    const ext = path.extname(req.url).toLowerCase();
+    let contentType;
 
-  else {
+    switch (ext) {
+      case ".jpg":
+      case ".jpeg":
+        contentType = "image/jpeg";
+        break;
+      case ".png":
+        contentType = "image/png";
+        break;
+      case ".gif":
+        contentType = "image/gif";
+        break;
+      case ".svg":
+        contentType = "image/svg+xml";
+        break;
+      default:
+        contentType = "application/octet-stream"; // Default content type
+    }
+
+    serveFile(res, req.url, contentType);
+  } else {
     // Serve 404 for unknown routes
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("404 Not Found");
   }
 });
-
 // Start the server
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
