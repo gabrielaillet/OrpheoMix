5151b196944698e42a8ee4d7b7151945d57bcb10
alicereyy <128583531+alicereyy@users.noreply.github.com>
2025-01-21 16:46:01 +0100

    add delete button, doesnt work

--- server/startServer.js
+++ server/startServer.js
@@ -285,6 +285,23 @@ app.post("/playlists/:userId", (req, res) => {
   )
 })
 
+app.post("/playlists/:userId/delete", (req, res) => {
+  const { playlistId } = req.body;
+  db.run(
+    'DELETE FROM playlists WHERE id = ?',
+    [playlistId],
+    function (err) {
+      if (err) {
+        console.error("Error deleting playlist:", err.message);
+        return res.status(500).send("Error deleting playlist.");
+      }
+      res
+        .status(200)
+        .send({ message: "Playlist deleted successfully" });
+    }
+  );
+});
+
 // First, add a new database connection for artists
 const dbArtists = new sqlite3.Database("db/artists.db", (err) => {
   if (err) {
